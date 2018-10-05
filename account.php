a9031991f59f895de1199beb614e8ddae68dde5d
Knapsack123 <43889909+Knapsack123@users.noreply.github.com>
2018-10-05 23:17:55 +0530

    Update account.php

--- account.php
+++ account.php
@@ -2,14 +2,14 @@
 <Head>
 	<Title>Account - Can't stop the Riats</Title>
 	<link rel = "stylesheet" type = "text/css" href = "layout.css"/>
-	<link rel="icon" type="image/png" href="favicon.png">
+<link rel="icon" type="image/png" href="favicon.png">
 </Head>
 <Body>
 	<?php include "header.php"; ?>
-
 	<div id = "account">
 		<?php echo "$email"; ?>
 	</div>
 	<?php include "footer.php"; ?>
+	Math.random();
 </Body>
 </HTML>
